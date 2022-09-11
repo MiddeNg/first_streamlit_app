dd344e7da16622ed9f84125f714e9e26d42669dc
William Ng <61858623+MiddeNg@users.noreply.github.com>
2022-09-11 14:27:44 +0800

    Update streamlit_app.py
    add pre-selected fruit to multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strewberries')
