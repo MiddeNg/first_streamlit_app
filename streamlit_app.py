7cf8881039c0869eb9e44988c0f4edefba88fb7f
William Ng <61858623+MiddeNg@users.noreply.github.com>
2022-09-11 14:23:15 +0800

    Update streamlit_app.py
    import panda and show dataframe of fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-import streamlit
+import streamlit, pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 streamlit.title('🥣 My Parent New Healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
