37dbdd3da8c93973c7cf1ee9e7d0373fb93a4d38
William Ng <61858623+MiddeNg@users.noreply.github.com>
2022-09-11 14:28:20 +0800

    Update streamlit_app.py
    fixed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strewberries'])
 streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strewberries')
