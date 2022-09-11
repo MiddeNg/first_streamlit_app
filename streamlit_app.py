15539bb0d2137375addb3523e1becf98d288c008
William Ng <61858623+MiddeNg@users.noreply.github.com>
2022-09-11 14:31:58 +0800

    Update streamlit_app.py
    show selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruit selectdd = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
